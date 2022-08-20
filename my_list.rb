02e9548af4bf534b8d4f3dcccfc97df0fa229c28
Paul Onchera <hesbonpoisson@gmail.com>
2022-08-20 16:46:53 +0300

    Make changes as requested by the reviewer

--- my_list.rb
+++ my_list.rb
@@ -4,6 +4,17 @@ class MyList
   def initialize(*list)
     @list = list
   end
+
+  def each
+    return to_enum(:each) unless block_given?
+
+    counter = 0
+    while counter < @list.length
+      yield(@list[counter])
+      counter += 1
+    end
+    @list
+  end
 end
 
 list = MyList.new(1, 2, 3, 4)
