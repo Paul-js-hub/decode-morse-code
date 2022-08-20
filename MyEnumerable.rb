142ddde55c889a88e5a7bbb9a81342c7c0d39bf3
Paul Onchera <hesbonpoisson@gmail.com>
2022-08-20 16:00:10 +0300

    Create enumerable

---
+++ MyEnumerable.rb
@@ -0,0 +1,31 @@
+module MyEnumerable
+  def list
+    @list ||= []
+  end
+
+  def all?
+    @list.each do |element|
+      return true unless block_given?
+      true_false = yield(element) 
+      return false unless true_false
+    end
+    return true
+  end
+
+  def any?
+    @list.each do |element|
+      return false unless block_given?
+      true_false = yield(element) 
+      return true if true_false
+    end
+    return false
+  end
+
+  def filter
+    array = []
+    @list.filter do |element|
+      array.push(element) if yield element
+    end
+    array
+  end
+end
